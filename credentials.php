fdee2eaa2714890996fe7eb77b555d77e2e6a99f
spectrefvm <rtfhffjfhtmimi@googlemail.com>
2025-02-11 20:32:36 +0100

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,4 +1,4 @@
 <?php
-$name = ""; // the name of your application
-$OwnerId = ""; // can be found in your account settings
-$SellerKey = ""; // application's seller key. Found in seller settings (You must have seller plan)
+$name = "ColdService"; // the name of your application
+$OwnerId = "d2VV5KGdh3"; // can be found in your account settings
+$SellerKey = "f11a05899fec74c0d862083d379021c6"; // application's seller key. Found in seller settings (You must have seller plan)
